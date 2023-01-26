b11b1a9b0baca1cd44f1a04cf8bda61d560c4a77
Exxuslee <91946073+Exxuslee@users.noreply.github.com>
2023-01-26 13:07:52 +0200

    -package

--- app/src/main/java/com/cookiejarapps/android/smartcookieweb/BrowserDirection.kt
+++
@@ -1,7 +0,0 @@
-package com.cookiejarapps.android.smartcookieweb
-
-import androidx.annotation.IdRes
-
-enum class BrowserDirection(@IdRes val fragmentId: Int) {
-    FromGlobal(0),
-}
--- app/src/main/java/com/cookiejarapps/android/smartcookieweb/browser/HomepageChoice.kt
+++
@@ -1,7 +0,0 @@
-package com.cookiejarapps.android.smartcookieweb.browser
-
-enum class HomepageChoice {
-    VIEW,
-    BLANK_PAGE,
-    CUSTOM_PAGE
-}
